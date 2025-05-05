3d8cb1a516bbe57e2328073122b46d8ae5de9a19
sohag47 <minhazul.islamcse@gmail.com>
2025-05-05 22:05:52 +0600

    class

---
+++ learning_CSharp/Basic.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_CSharp
+{
+    internal class Basic
+    {
+        // Read and Write Statement
+        //Console.WriteLine("Enter your name:");
+        //string? name = Console.ReadLine();
+        //Console.WriteLine($"My name is {name}");
+
+
+        // data types
+        //const string employeer = "ILT";
+        //string name = "Minhazu Islam Sohag";
+        //int age = 10;
+        //bool isEmployee = false;
+        //double salary = 30000;
+
+        //Console.WriteLine($"Hi, My Name is {name}. I am {age} years old");
+        //if (isEmployee)
+        //{
+        //    Console.WriteLine($"Is Employee {isEmployee}. and salary is {salary} at {employeer}");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"{name} is Student");
+        //}
+
+        //Console.WriteLine(isEmployee ? "Employee" : "Student");
+
+        //int day = 3;
+        //switch (day)
+        //{
+        //    case 0:
+        //        Console.WriteLine("Sunday");
+        //        break;
+        //    case 1:
+        //        Console.WriteLine("Monday");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Tuesday");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine("Wednesday");
+        //        break;
+        //    default:
+        //        Console.WriteLine("No Data Found!");
+        //        break;
+        //}
+
+        //ArrayOperations.Show();
+    }
+}
--- learning_CSharp/Program.cs
+++ learning_CSharp/Program.cs
@@ -1,62 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace learning_CSharp
 {
+    class Person
+    {
+        public int id;
+        public string name;
+        public int age;
+
+        public Person(int _id, string _name, int _age)
+        {
+            id = _id;
+            name = _name;
+            age = _age;
+        }
+        public void Profile()
+        {
+            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}");
+        }
+    }
     class Program
     {
         static void Main()
         {
-            // Read and Write Statement
-            //Console.WriteLine("Enter your name:");
-            //string? name = Console.ReadLine();
-            //Console.WriteLine($"My name is {name}");
+            Person user1 = new Person(1, "Minhazul", 10);
+            user1.Profile();
+            
 
-
-            // data types
-            //const string employeer = "ILT";
-            //string name = "Minhazu Islam Sohag";
-            //int age = 10;
-            //bool isEmployee = false;
-            //double salary = 30000;
-
-            //Console.WriteLine($"Hi, My Name is {name}. I am {age} years old");
-            //if (isEmployee)
-            //{
-            //    Console.WriteLine($"Is Employee {isEmployee}. and salary is {salary} at {employeer}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"{name} is Student");
-            //}
-
-            //Console.WriteLine(isEmployee ? "Employee" : "Student");
-
-            //int day = 3;
-            //switch (day)
-            //{
-            //    case 0:
-            //        Console.WriteLine("Sunday");
-            //        break;
-            //    case 1:
-            //        Console.WriteLine("Monday");
-            //        break;
-            //    case 2:
-            //        Console.WriteLine("Tuesday");
-            //        break;
-            //    case 3:
-            //        Console.WriteLine("Wednesday");
-            //        break;
-            //    default:
-            //        Console.WriteLine("No Data Found!");
-            //        break;
-            //}
-
-            //ArrayOperations.Show();
-            StringOperation.Show();
+            Person user2 = new(2, "Miraz", 30);
+            user2.Profile();
+           
         }
     }
 }
