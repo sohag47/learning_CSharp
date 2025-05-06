16b18075b9451cc1cda3cfdfab05dff9dd07047f
sohag47 <minhazul.islamcse@gmail.com>
2025-05-06 21:11:51 +0600

    change class name

---
+++ learning_CSharp/OOP/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_CSharp.OOP
+{
+    class Person
+    {
+        public int id;
+        public string name;
+        public int age;
+
+        public Person(int _id, string _name, int _age)
+        {
+            id = _id;
+            name = _name;
+            age = _age;
+        }
+        public void Profile()
+        {
+            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}");
+        }
+    }
+}
--- learning_CSharp/Program.cs
+++ learning_CSharp/Program.cs
@@ -4,26 +4,11 @@ using System.Linq;
 using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
+using learning_CSharp.OOP;
 
 namespace learning_CSharp
 {
-    class Person
-    {
-        public int id;
-        public string name;
-        public int age;
 
-        public Person(int _id, string _name, int _age)
-        {
-            id = _id;
-            name = _name;
-            age = _age;
-        }
-        public void Profile()
-        {
-            Console.WriteLine($"ID: {id}, Name: {name}, Age: {age}");
-        }
-    }
     class Program
     {
         static void Main()
